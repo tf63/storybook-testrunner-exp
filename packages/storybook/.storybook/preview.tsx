64a5f073f3d3822dd8e7377921742b58363a91a8
tf63 <74246282+tf63@users.noreply.github.com>
2024-09-14 22:09:17 +0900

    feat: setup the addon for storybook coverage

--- packages/storybook/.storybook/preview.tsx
+++ packages/storybook/.storybook/preview.tsx
@@ -2,12 +2,21 @@ import type { Decorator, Preview } from '@storybook/react'
 
 import '@repo/tailwind/styles.css'
 
+declare global {
+    var __coverage__: string
+}
+
 export const decorators: Decorator[] = [
     (Story) => (
         <div className="flex items-center justify-center p-5">
             <Story />
         </div>
-    )
+    ),
+    (Story) => {
+        // biome-ignore lint/suspicious/noConsoleLog: <explanation>
+        console.log(globalThis.__coverage__)
+        return Story()
+    }
 ]
 
 const preview: Preview = {
